dd41951832f7f8174212d3406356fe4a06f6d6e5
Vincent Nguyen <HNguyen@beachbody.com>
2018-03-12 14:56:56 -0700

    Update test.php

--- test.php
+++ test.php
@@ -190,6 +190,11 @@ class cURLHandler {
 <html>
   <head>
     <title>Mocha Tests</title>
+	
+	  <script
+  src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
     
   </head>
   <body>
